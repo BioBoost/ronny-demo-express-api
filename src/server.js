843434216458e0a94d4c2edace4007c273be17b8
Nico De Witte <nico.dewitte@outlook.com>
2023-03-08 10:53:37 +0100

    Demo of rest API

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+
+// parse application/json
+app.use(bodyParser.json());
+
+let sensors = [
+  { id: 1, type: 'temperature' },
+  { id: 2, type: 'moisture' }
+];
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World!' });
+});
+
+app.post('/sensors', (req, res) => {
+  console.log(req.body);
+  sensors.push(req.body);
+  res.send(req.body);
+});
+
+app.get('/sensors', (req, res) => {
+  res.send(sensors);
+});
+
+app.get('/sensors/:id', (req, res) => {
+  const sensor = sensors.find(sensor => sensor.id === parseInt(req.params.id))
+  if (!sensor) res.status(404).send({ error: 'The sensor with the given ID was not found.'});
+  res.send(sensor);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
